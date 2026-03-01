d1f914a867646c2c71b9b6ae1ccd9784d8ca14cb
Vinicius Ribeiro <Viniciusrsp7@gmail.com>
2026-03-01 15:55:10 -0300

    Exercicios Estrutura Repetitiva PART 2

--- EstruturasRepetitivas/ExerciciosEstruturasRepetitivas/src/Ex001.java
+++ EstruturasRepetitivas/ExerciciosEstruturasRepetitivas/src/Ex001.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ex001 {
 
-    public static void main() {
+    public static void main(String[] args) {
 
         Scanner sc  = new Scanner(System.in);
 
---
+++ EstruturasRepetitivas/ExerciciosEstruturasRepetitivas/src/Ex010.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex010 {
+
+    public static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+
+            int primeiro = i;
+            int segundo = i * i;
+            int terceiro = i * i * i;
+            System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+        }
+
+        sc.close();
+    }
+}
